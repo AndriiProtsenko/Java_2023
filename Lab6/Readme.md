1167b55637702971fdba41d1cb481bb057d4afe6
unknown <protsenko.andrii@lll.kpi.ua>
2023-10-30 23:17:49 +0200

    lab6

---
+++ Lab6/Readme.md
@@ -0,0 +1,13 @@
+# Laba6
+**The work was performed by Andrii Protsenko.**
+
+ **Variant 18**
+
+In this work, we wrote code that converts a String representing a number in the octal numeral system into an int value, and code for counting the number of combinations of letters and digits.
+
+**Resalts:**
+
+- Task6: (for String: 10); int: 8;
+- Task9: (for string: sdfs 3e adda s3 3 f); Кiлькiсть комбiнацiй лiтер та цифр: 2;
+
+
--- Lab6/lab6_task9.java
+++ Lab6/lab6_task9.java
@@ -1,6 +1,6 @@
 public class lab6_task9 {
     public static void main(String[] args) {
-        String input = "dsfdfs df s3 afa 2d 2 3d fd";
+        String input = "The user with the nickname koala757677 this month wrote 3 times more comments than the user with the nickname croco181dile920 4 months ago";
         int count = countLetterNumberCombinations(input);
         System.out.println("Кiлькiсть комбiнацiй лiтер та цифр: " + count);
     }
