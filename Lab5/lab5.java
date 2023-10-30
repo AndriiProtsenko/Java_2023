72d87da8d2da3d964459339d35b470627e71f5e6
unknown <protsenko.andrii@lll.kpi.ua>
2023-10-30 19:26:46 +0200

    Update lab5.java

--- Lab5/lab5.java
+++ Lab5/lab5.java
@@ -12,7 +12,7 @@ public class lab5 {
         for (int i = 1; i < arr.length; i++) {
             long key = arr[i];
             int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
+            while (j >= 0 && arr[j] < key) {
                 arr[j + 1] = arr[j];
                 j--;
             }
@@ -25,7 +25,7 @@ public class lab5 {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+                if (arr[j] < arr[j + 1]) {
                     long temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
