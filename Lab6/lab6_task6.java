24137e081fc9cfcce168f8556a5800192c5addf2
unknown <protsenko.andrii@lll.kpi.ua>
2023-10-30 20:14:48 +0200

    Create lab6_task6.java

---
+++ Lab6/lab6_task6.java
@@ -0,0 +1,25 @@
+public class lab6_task6 {
+    public static void main(String[] args) {
+        String octalString = "10";
+        int decimalNumber = octStringToInt(octalString);
+        System.out.println(decimalNumber);
+    }
+
+    public static int octStringToInt(String s) {
+        s = s.toLowerCase(); // Перетворення рядка у нижній регістр (не обов'язково, але робить код більш гнучким)
+
+        int decimalNumber = 0;
+
+        char[] chars = s.toCharArray();
+        for (char digit : chars) {
+            int octalDigit = Character.getNumericValue(digit);
+            if (octalDigit >= 0 && octalDigit <= 7) { // Перевірка, що символ є правильною цифрою вісімкової системи
+                decimalNumber = decimalNumber * 8 + octalDigit;
+            } else {
+                throw new IllegalArgumentException("Вхiдний рядок не є правильним числом у вiсiмковiй системi.");
+            }
+        }
+
+        return decimalNumber;
+    }
+}
